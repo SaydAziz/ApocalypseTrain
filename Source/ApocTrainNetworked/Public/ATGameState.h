fe75c484540e670976dc1e537e8f773485bb7822
Mack Pearson <chopov1@gmail.com>
2024-06-16 22:37:27 -0500

    Rename Existing Test Blueprints - Add Cpp and Reparent BP for GameState PlayerCharacter PlayerManager

---
+++ Source/ApocTrainNetworked/Public/ATGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "ATGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APOCTRAINNETWORKED_API AATGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
