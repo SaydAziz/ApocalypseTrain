6ae9f241f3e05f07ddbb66881e1abbede1a2b425
Mack Pearson <chopov1@gmail.com>
2024-07-16 22:07:57 -0500

    Add basic obstacle and IDamagable

---
+++ Source/ApocTrainNetworked/Private/Obstacle.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Obstacle.h"
+
+// Sets default values
+AObstacle::AObstacle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	SetReplicates(true);
+}
+
+// Called when the game starts or when spawned
+void AObstacle::BeginPlay()
+{
+	Super::BeginPlay();
+	currentHealth = MaxHealth;
+}
+
+// Called every frame
+void AObstacle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AObstacle::Damage(float damageToTake)
+{
+	currentHealth -= damageToTake;
+	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("health: %f"), currentHealth));
+	if (currentHealth <= 0) {
+		Destroy(0);
+	}
+}
+
+float AObstacle::GetHealth()
+{
+	return currentHealth;
+}
+
---
+++ Source/ApocTrainNetworked/Public/Obstacle.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Damagable.h"
+
+#include "Obstacle.generated.h"
+
+UCLASS()
+class APOCTRAINNETWORKED_API AObstacle : public AActor, public IDamagable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AObstacle();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float MaxHealth;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	float currentHealth;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Damage(float damageToTake);
+
+	virtual float GetHealth();
+
+};
